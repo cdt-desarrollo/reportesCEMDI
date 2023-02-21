6135d4bfffc46b80b06a81c2919011b7b7c6582d
Jair Hernández <startupstijuana@gmail.com>
2023-02-20 16:30:16 -0800

    Update

--- main.js
+++ main.js
@@ -8,28 +8,28 @@ function inicio(){
   <div class="container" id="toEraseAndAddContentDiv">
       <div class="row row-cols-2">
         <div class="col-md-auto card" style="width: 18rem;">
-          <img src="/assets/octubre2022.png" class="card-img-top" alt="...">
+          <img src="assets/octubre2022.png" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Octubre del 2022</h5>
             <a href="#" class="btn btn-primary" onclick="deleteDiv('Octubre del 2022')">Ver reporte</a>
           </div>
         </div>
         <div class="col-md-auto card" style="width: 18rem;">
-          <img src="/assets/noviembre2022.png" class="card-img-top" alt="...">
+          <img src="assets/noviembre2022.png" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Noviembre del 2022</h5>
             <a href="#" class="btn btn-primary" onclick="deleteDiv('Noviembre del 2022')">Ver reporte</a>
           </div>
         </div>
         <div class="col-md-auto card" style="width: 18rem;">
-          <img src="/assets/noviembre2022.png" class="card-img-top" alt="...">
+          <img src="assets/noviembre2022.png" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Diciembre del 2022</h5>
             <a href="#" class="btn btn-primary" onclick="deleteDiv('Diciembre del 2022')">Ver reporte</a>
           </div>
         </div>
         <div class="col-md-auto card" style="width: 18rem;">
-          <img src="/assets/enero2023.png" class="card-img-top" alt="...">
+          <img src="assets/enero2023.png" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Enero del 2023</h5>
             <a href="#" class="btn btn-primary" onclick="deleteDiv('Enero del 2023')">Ver reporte</a>
@@ -47,7 +47,6 @@ function deleteDiv(month){
     document.getElementById("toEraseAndAddContentDiv").innerHTML = `<h1 class="text-center">${month}</h1>
     <div class="d-flex justify-content-center align-self-center">
     <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/167c0345-b2e9-48c8-8eca-5128d05f5f31?startpage=1&allowFullscreen=true" width="${width}" height="${height}" frameborder="0" allowfullscreen="" id="iFrame1"></iframe>
-
     </div>
     `;
   }
