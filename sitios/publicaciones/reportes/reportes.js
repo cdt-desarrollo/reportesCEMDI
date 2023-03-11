a47a7d35237e916d92f7664af224c22fdad80c1d
Jair Hernández <startupstijuana@gmail.com>
2023-03-10 17:20:31 -0800

    Update

--- sitios/publicaciones/reportes/reportes.js
+++ sitios/publicaciones/reportes/reportes.js
@@ -5,7 +5,7 @@ function showReport(month){
   console.log(month)
   if(month == "enero2023"){
     Swal.fire({
-      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/fac465e0-cafd-4e9d-8f88-0a805e19518c?startpage=1&allowFullscreen=false" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/fac465e0-cafd-4e9d-8f88-0a805e19518c?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
       imageWidth: width,
       imageHeight: height,
       imageAlt: 'A tall image'
