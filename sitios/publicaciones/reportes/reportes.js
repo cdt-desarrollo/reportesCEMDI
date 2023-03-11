a5a55c058f9141e81a5fc5ec4918e56d4591c1e1
Jair Hernández <startupstijuana@gmail.com>
2023-03-10 19:10:47 -0800

    Update

--- sitios/publicaciones/reportes/reportes.js
+++ sitios/publicaciones/reportes/reportes.js
@@ -19,4 +19,60 @@ function showReport(month){
       imageAlt: 'A tall image'
     })
   }
+  else if(month == "junio2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/b3968f15-1134-4798-9554-00a5e65b944b?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "julio2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/80136f12-a6e8-4b5b-93ba-e906ccf7e602?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "agosto2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/1451e471-19e5-46de-a36e-01349425c0aa?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "septiembre2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/f2462565-52db-40fd-b605-875abe4c7d3a?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "octubre2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/167c0345-b2e9-48c8-8eca-5128d05f5f31?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "noviembre2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/9b95b3b2-163b-48b6-beac-0c2d67f0b26e?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "diciembre2022"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/00c9b805-9751-4876-86b5-beffab24f682?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
 }
