1e5718276722a8dcabc30642226ae2c2b43811c7
Jair Hernández <startupstijuana@gmail.com>
2023-04-05 14:03:02 -0700

    Update

--- sitios/publicaciones/reportes/reportes.js
+++ sitios/publicaciones/reportes/reportes.js
@@ -5,7 +5,7 @@ function showReport(month){
   console.log(month)
   if(month == "enero2023"){
     Swal.fire({
-      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/fac465e0-cafd-4e9d-8f88-0a805e19518c?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      html: '<iframe style="border: 1px solid #777;" src="https://drive.google.com/file/d/1UnshWaYbGvexEZRIqHJJPQs6ob52OhMm/preview" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
       imageWidth: width,
       imageHeight: height,
       imageAlt: 'A tall image'
@@ -13,7 +13,15 @@ function showReport(month){
   }
   else if(month == "febrero2023"){
     Swal.fire({
-      html: '<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/e6dbff7a-736c-4364-bf26-54e652d75d72?startpage=1&allowFullscreen=true" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      html: '<iframe style="border: 1px solid #777;" src="https://drive.google.com/file/d/1UnssGqR38glN2FnyicErd5YSMip4AILw/preview" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
+      imageWidth: width,
+      imageHeight: height,
+      imageAlt: 'A tall image'
+    })
+  }
+  else if(month == "marzo2023"){
+    Swal.fire({
+      html: '<iframe style="border: 1px solid #777;" src="https://drive.google.com/file/d/1UoYd61VveCiloQJ2X2L0bVFYpxUNsZi7/preview" width="450px" height="760px" frameborder="0" allowfullscreen=""></iframe>',
       imageWidth: width,
       imageHeight: height,
       imageAlt: 'A tall image'
