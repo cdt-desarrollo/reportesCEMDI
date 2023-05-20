d9ea07922d91e844eb38cc521c562223ef0d188d
Jair Hernández <startupstijuana@gmail.com>
2023-05-19 19:35:30 -0700

    Update
    Showing the first charts and buttons to trigger the get petition and display a button to download data

--- sitios/estadisticas/estadisticas.js
+++ sitios/estadisticas/estadisticas.js
@@ -1,23 +1,135 @@
+periods = 40
+arrayIED = []
+arrayRemittances = []
+arrayInflationBC = []
+google.charts.load('current', {'packages':['corechart']});
 
-// google.charts.load('current', {'packages':['corechart']});
-//       google.charts.setOnLoadCallback(drawChart);
+async function getDataIEDIndicator(){
+  var config = {
+    method: 'get',
+    url: 'https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/indicador_IED',
+  }
+  await axios(config)
+  .then((res) => {
+    for(let i = res.data.length; i >= res.data.length - periods; i--){
+      arrayIED.push(res.data[i])
+    }
+    arrayIED.shift()
+    arrayIED.reverse()
+    google.charts.setOnLoadCallback(drawIEDIndicator);
+  })
+  .catch(async (err) => {
+    console.log(err)
+  })
+}
+async function drawIEDIndicator() {
+  // Crea un DataTable y agrega los datos
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', '');
+  data.addColumn('number', '');
+  for (var i = 0; i < arrayIED.length; i++) {
+    data.addRow([arrayIED[i].date, parseFloat(arrayIED[i].bajaCalifornia)]);
+  }
 
-//       function drawChart() {
-//         var data = google.visualization.arrayToDataTable([
-//           ['Year', 'Sales', 'Expenses'],
-//           ['2004',  1000,      400],
-//           ['2005',  1170,      460],
-//           ['2006',  660,       1120],
-//           ['2007',  1030,      540]
-//         ]);
+  // Configuración del gráfico
+  var options = {
+    curveType: 'function',
+    legend: 'none',
+  };
 
-//         var options = {
-//           title: 'Company Performance',
-//           curveType: 'function',
-//           legend: { position: 'bottom' }
-//         };
+  // Crea un gráfico de líneas y lo dibuja en el elemento con el ID "chart_div"
+  var chart = new google.visualization.LineChart(document.getElementById('IEDChart'));
+  chart.draw(data, options);
+  document.getElementById("buttonIED").style.display = "none"
+  document.getElementById("buttonDownloadIEDBC").style.display = "inline-block"
+}
+async function getDataRemittancesIndicator(){
+  var config = {
+    method: 'get',
+    url: 'https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/indicador_Remesas',
+  }
+  await axios(config)
+  .then((res) => {
+    for(let i = res.data.length; i >= res.data.length - periods; i--){
+      arrayRemittances.push(res.data[i])
+    }
+    arrayRemittances.shift()
+    arrayRemittances.reverse()
+    console.log(arrayRemittances)
+    google.charts.setOnLoadCallback(drawRemittancesIndicator);
+  })
+  .catch(async (err) => {
+    console.log(err)
+  })
+}
+async function drawRemittancesIndicator() {
+  // Crea un DataTable y agrega los datos
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Fecha');
+  data.addColumn('number', 'Indicador');
+  for (var i = 0; i < arrayRemittances.length; i++) {
+    data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].bajaCalifornia)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].ensenada)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].mexicali)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].playasDeRosarito)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].tecate)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].tijuana)]);
+  }
 
-//         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+  // Configuración del gráfico
+  var options = {
+    curveType: 'function',
+    legend: 'none',
+  };
 
-//         chart.draw(data, options);
-//       }
+  // Crea un gráfico de líneas y lo dibuja en el elemento con el ID "chart_div"
+  var chart = new google.visualization.LineChart(document.getElementById('remesasChart'));
+  chart.draw(data, options);
+  document.getElementById("buttonRemesas").style.display = "none"
+  document.getElementById("buttonDownloadRemittancesBC").style.display = "inline-block"
+}
+async function getDataInflationBCIndicator(){
+  var config = {
+    method: 'get',
+    url: 'https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/indicador_inflacionMensualBC',
+  }
+  await axios(config)
+  .then((res) => {
+    for(let i = res.data.length; i >= res.data.length - periods; i--){
+      arrayInflationBC.push(res.data[i])
+    }
+    arrayInflationBC.shift()
+    arrayInflationBC.reverse()
+    console.log(arrayInflationBC)
+    google.charts.setOnLoadCallback(drawInflationBCIndicator);
+  })
+  .catch(async (err) => {
+    console.log(err)
+  })
+}
+async function drawInflationBCIndicator() {
+  // Crea un DataTable y agrega los datos
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Fecha');
+  data.addColumn('number', 'Indicador');
+  for (var i = 0; i < arrayInflationBC.length; i++) {
+    data.addRow([arrayInflationBC[i].date, parseFloat(arrayInflationBC[i].bajaCalifornia)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].ensenada)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].mexicali)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].playasDeRosarito)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].tecate)]);
+    // data.addRow([arrayRemittances[i].date, parseFloat(arrayRemittances[i].tijuana)]);
+  }
+
+  // Configuración del gráfico
+  var options = {
+    curveType: 'function',
+    legend: 'none',
+  };
+
+  // Crea un gráfico de líneas y lo dibuja en el elemento con el ID "chart_div"
+  var chart = new google.visualization.LineChart(document.getElementById('inflationBCChart'));
+  chart.draw(data, options);
+  document.getElementById("buttonInflationBC").style.display = "none"
+  document.getElementById("buttonDownloadInflationBC").style.display = "inline-block"
+}
